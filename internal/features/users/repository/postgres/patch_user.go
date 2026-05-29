aacc595c75e684ff72a2a807355ae9e2487edc34
eva0101 <ke.kupkup44@mail.ru>
2026-05-29 11:31:41 +0500

    fixes

--- internal/features/users/repository/postgres/patch_user.go
+++ internal/features/users/repository/postgres/patch_user.go
@@ -7,7 +7,7 @@ import (
 
 	"github.com/eva0101/golang-todoapp/internal/core/domain"
 	core_errors "github.com/eva0101/golang-todoapp/internal/core/errors"
-	"github.com/jackc/pgx/v5"
+	core_postgres_pool "github.com/eva0101/golang-todoapp/internal/core/repository/postgres/pool"
 )
 
 func (r *UsersRepository) PatchUser(
@@ -29,7 +29,7 @@ func (r *UsersRepository) PatchUser(
 		id,
 		version,
 		full_name,
-		phone_number
+		phone_number;
 	`
 
 	row := r.pool.QueryRow(
@@ -49,7 +49,7 @@ func (r *UsersRepository) PatchUser(
 		&userModel.PhoneNumber,
 	)
 	if err != nil {
-		if errors.Is(err, pgx.ErrNoRows) {
+		if errors.Is(err, core_postgres_pool.ErrNoRows) {
 			return domain.User{}, fmt.Errorf(
 				"user with id='%d' concurrently accessed: %w",
 				id,
